05068ee2aca483a04498a095804f2f2f447212a3
ldcss <lucass.dcss75@gmail.com>
2025-03-12 22:47:10 -0300

    feat: add proxy to solve CORS request error

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,20 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tsconfigPaths from "vite-tsconfig-paths"
+import mkcert from 'vite-plugin-mkcert'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
-})
+  plugins: [react(), tsconfigPaths(), mkcert()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://openrouter.ai/api',
+        changeOrigin: true,
+        secure: false,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      }
+    }
+    },
+  },
+)
