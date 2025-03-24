9cf8abf8dbbdc1f15027b7316fe452123c65e227
ldcss <lucass.dcss75@gmail.com>
2025-03-24 13:56:47 -0300

    refactor: make baseurl dinamic

--- src/service/api.ts
+++ src/service/api.ts
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
-const api = axios.create({baseURL:  import.meta.env.MODE == 'production' ? 'https://welfarec.vercel.app/api' : '/api', withCredentials: true });
+const api = axios.create({baseURL:  import.meta.env.MODE == 'production' ? `${window.origin}/api` : '/api', withCredentials: true });
 
 export default api;
